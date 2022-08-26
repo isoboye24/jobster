12a98801237e3ca1eca9a0775a2d023a5dfe5e2c
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-08-26 15:27:08 +0200

    I setup the structure of the landing page and edited the index.scss file.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,29 @@ import logo from "./../assets/images/logo.svg";
 import main from "./../assets/images/main.svg";
 
 const Landing = () => {
-  return <h1>Landing page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="Jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus
+            dolore aliquid cumque itaque eos iusto corporis, expedita minus
+            tempora deleniti eligendi blanditiis sequi nam corrupti libero in
+            repudiandae non eveniet.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="Job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
