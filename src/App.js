8b1b01b08c980b2d72bb0d548b3041968cc3e904
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-08-26 15:47:45 +0200

    I installed the styled components
    I also deleted css file and did a basic setup of the styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import Landing from "./pages/Landing";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 3rem;
+`;
+const SecondButton = styled.button`
+  background: blue;
+  color: white;
+  font-size: 2rem;
+`;
 
 function App() {
   return (
     <div>
+      <Button>Click me</Button>
+      <SecondButton>Click me</SecondButton>
       <Landing />
     </div>
   );
