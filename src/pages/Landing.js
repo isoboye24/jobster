55693453ef5d1499f6ca8df1098591cf0c8b9b5f
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-08-26 23:02:29 +0200

    I renamed the wrapper folder and used the styling of the landingPage.js

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,47 +1,6 @@
 import logo from "./../assets/images/logo.svg";
 import main from "./../assets/images/main.svg";
-import styled from "styled-components";
-
-const Wrapper = styled.main`
-  nav {
-    margin: 0 auto;
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh-var(--nav-height));
-    display: grid;
-    align-items: center;
-    /* margin-top: -1rem; */
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-
-  p {
-    color: var(--grey-600);
-  }
-
-  .main-img {
-    display: none;
-  }
-
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
+import Wrapper from "../assets/css_structures/LandingPage";
 
 const Landing = () => {
   return (
