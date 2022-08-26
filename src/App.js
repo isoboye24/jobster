a173c4b5ff1e373fea84e580e3d97e9158484383
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-08-26 22:50:51 +0200

    I styledcthe landing page.
    I used the css file because the styled-component does not work with scss variables but rather css variables.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import Landing from "./pages/Landing";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 3rem;
-`;
-const SecondButton = styled.button`
-  background: blue;
-  color: white;
-  font-size: 2rem;
-`;
 
 function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-      <SecondButton>Click me</SecondButton>
-      <Landing />
-    </div>
+    <>
+      <Landing />;
+    </>
   );
 }
 
