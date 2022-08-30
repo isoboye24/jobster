241d362aee7e12335e28a97e25eed56742ebeaa1
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-08-30 18:47:26 +0200

    I created the user slice and store files.
    I installed react-redux and @reduxjs/toolkit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import "normalize.css";
 import "./index.scss";
 import "./index.css";
 import App from "./App";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
