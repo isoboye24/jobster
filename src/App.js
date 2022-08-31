fbc655484ca6e8d291037d7383625c73a8c83e68
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-08-31 23:35:51 +0200

    I setup the dashboard pages and updated the route.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { Landing, Error, Register, Dashboard } from "./pages";
+import { Landing, Error, Register } from "./pages";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  Stats,
+  SharedLayout,
+} from "./pages/dashboard";
+
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-jobs" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
---
+++ src/pages/dashboard/AddJob.js
@@ -0,0 +1,5 @@
+const AddJobs = () => {
+  return <h1>AddJobs</h1>;
+};
+
+export default AddJobs;
