844430129717383aa8499b5d20985e60b76fb182
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-02 18:35:34 +0200

    I setup the logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Logo from "./Logo";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleSidebar } from "../features/user/userSlice";
+import { toggleSidebar, logoutUser } from "../features/user/userSlice";
 
 const Navbar = () => {
   const { user } = useSelector((state) => state.user);
@@ -36,7 +36,7 @@ const Navbar = () => {
             <button
               type="button"
               className="dropdown-btn"
-              onClick={() => console.log("User logout")}
+              onClick={() => dispatch(logoutUser())}
             >
               Logout
             </button>
