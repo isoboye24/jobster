2c296616bfbff214e103d18e1ac1d0b293150407
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-02 15:30:34 +0200

    I setup the navbar structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,46 @@
 import Wrapper from "../assets/css_structures/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Logo from "./Logo";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   return (
     <Wrapper>
-      <h2>Navbar</h2>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("Toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("Toggle logout dropdown")}
+          >
+            <FaUserCircle />
+            {user?.name} <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("User logout")}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
