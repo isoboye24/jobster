fe8d820509adb353da4201161954934f3f90c21d
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-02 15:50:33 +0200

    I setup the toggle dropdown logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ import { toggleSidebar } from "../features/user/userSlice";
 const Navbar = () => {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const [showLogout, setShowLogout] = useState(false);
 
   const toggle = () => {
     dispatch(toggleSidebar());
@@ -26,12 +27,12 @@ const Navbar = () => {
           <button
             type="button"
             className="btn"
-            onClick={() => console.log("Toggle logout dropdown")}
+            onClick={() => setShowLogout(!showLogout)}
           >
             <FaUserCircle />
             {user?.name} <FaCaretDown />
           </button>
-          <div className="dropdown show-dropdown">
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
             <button
               type="button"
               className="dropdown-btn"
