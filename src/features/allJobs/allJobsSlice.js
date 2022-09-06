950ae9b2e919f92d312aa3437fc5957503bb19c3
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-06 18:37:55 +0200

    I created the allJobsSlice

---
+++ src/features/allJobs/allJobsSlice.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import customFetch from "../../utils/axios";
+
+const initialFilterState = {
+  search: "",
+  searchStatus: "all",
+  searchType: "all",
+  sort: "latest",
+  sortOptions: ["latest", "oldest", "a-z", "z-a"],
+};
+
+const initialState = {
+  isLoading: false,
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
+  stats: {},
+  monthlyApplications: 0,
+  ...initialFilterState,
+};
+
+const allJobsSlice = createSlice({ name: "allJobs", initialState });
+
+export default allJobsSlice.reducer;
