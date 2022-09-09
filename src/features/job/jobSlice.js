b1493dfb35bd69c341f52a08360d9d81555b96f1
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-09 21:04:05 +0200

    I worked on the edit job functionality

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -71,6 +71,9 @@ const jobSlice = createSlice({
         jobLocation: getUserFromLocalStorage.getState()?.location || ``,
       };
     },
+    setEditJob: (state, { payload }) => {
+      return { ...state, isEditing: true, ...payload };
+    },
   },
   extraReducers: {
     // Response fo the create job
@@ -94,5 +97,5 @@ const jobSlice = createSlice({
   },
 });
 
-export const { handleChange, clearValues } = jobSlice.actions;
+export const { handleChange, clearValues, setEditJob } = jobSlice.actions;
 export default jobSlice.reducer;
