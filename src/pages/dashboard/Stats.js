c0cab51789d5ffe45f4e71f531b71980ac9c2853
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-10 15:43:28 +0200

    I setup the show stats request functionality

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { showStats } from "../../features/allJobs/allJobsSlice";
+import { useDispatch } from "react-redux";
+
 const Stats = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showStats());
+  }, []);
+
   return <h1>Stats</h1>;
 };
 
