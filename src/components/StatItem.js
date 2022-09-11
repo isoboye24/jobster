8ae5302174d16acf28c45e732427c2092bacec4f
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-12 00:18:10 +0200

    I setup the Stat Items.

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -1,5 +1,15 @@
-const StatItem = () => {
-  return <div>StatItem</div>;
+import Wrapper from "../assets/css_structures/StatItem";
+
+const StatItem = ({ title, count, icon, color, bcg }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
 };
 
 export default StatItem;
