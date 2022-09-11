5e01ecdcbf39ecd72b029e38dfbee114e95bf84f
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-12 01:20:12 +0200

    I worked on the chartContainer and installed recharts library

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Wrapper from "../assets/css_structures/ChartsContainer";
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
+
 const ChartsContainer = () => {
-  return <h2>ChartsContainer</h2>;
+  const [barChart, setBarChart] = useState(true);
+  const { applications: data } = useSelector((state) => state.allJobs);
+  return (
+    <Wrapper>
+      <h4>Monthly applications</h4>
+      <button
+        type="button"
+        onClick={() => {
+          setBarChart(!barChart);
+        }}
+      >
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart date={data} /> : <AreaChart date={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
