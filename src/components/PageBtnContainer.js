0e54df4d1179133043ad71a05bffd25a784e3d09
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2022-09-18 06:41:02 +0200

    I setup the page button container structure

--- src/components/PageBtnContainer.js
+++ src/components/PageBtnContainer.js
@@ -1,5 +1,46 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
+import Wrapper from "../assets/css_structures/PageBtnContainer";
+
 const PageBtnContainer = () => {
-  return <h2>PageBtnContainer</h2>;
+  const { page, numOfPages } = useSelector((state) => state.allJobs);
+  const dispatch = useDispatch();
+
+  const pages = Array.from({ length: numOfPages }, (_1, index) => {
+    return index + 1; // Since the index starts from 0, I added 1 to make it count from 1.
+  });
+
+  const nextPage = () => {
+    console.log("next page");
+  };
+  const prevPage = () => {
+    console.log("prev page");
+  };
+  return (
+    <Wrapper>
+      <button type="button" className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft /> Prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type="button"
+              key={pageNumber}
+              onClick={() => console.log("change page")}
+              className={pageNumber === page ? "pageBtn active" : "pageBtn"}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button type="button" className="next-btn" onClick={nextPage}>
+        Next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 };
 
 export default PageBtnContainer;
